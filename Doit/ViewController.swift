6e006bd0f9c89fed68cb08534ad1c775368aeb71
Sean Moorman <seanmoorman@seans-mbp.attlocal.net>
2017-07-09 12:13:32 -0500

    first commit, file created.

---
+++ Doit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Doit
+//
+//  Created by Sean Moorman on 7/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
